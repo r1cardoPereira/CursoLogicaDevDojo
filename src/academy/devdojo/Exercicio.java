b290efdac06809195548824fc5333f24b0ee30c4
Ricardo Pereira <103537193+r1cardoPereira@users.noreply.github.com>
2022-07-10 19:12:40 -0300

    Exercicio While e organização de arquivo nos pacotes

---
+++ src/academy/devdojo/Exercicio.java
@@ -0,0 +1,18 @@
+package src.academy.devdojo;
+
+import java.util.Scanner;
+
+public class Exercicio {
+    public static void main(String[] args) {
+        int a = 2;
+        while ( a < 1000){
+            a = a *2;
+            System.out.println(a);
+
+        }
+        System.out.println(a);
+
+
+
+    }
+}
