cc55d9be768657bd774f2af3211951bee0dda6c6
Ricardo Pereira <103537193+r1cardoPereira@users.noreply.github.com>
2022-08-01 11:39:45 -0300

    atualização
    .....

--- src/academy/devdojo/lacosderepeticao/repeticaofor/Ala01Introduction.java
+++ src/academy/devdojo/lacosderepeticao/repeticaofor/Ala01Introduction.java
@@ -1,6 +1,6 @@
 package src.academy.devdojo.lacosderepeticao.repeticaofor;
 
-public class Aula01Intriducao {
+public class Ala01Introduction {
     public static void main(String[] args) {
         for (int contador = 0; contador <=100; contador++ ){
             System.out.println(contador);
